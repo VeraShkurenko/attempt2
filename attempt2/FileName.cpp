c56342adfd85359606763c20eee814c9fe7a0337
Skur_pn81 <Skur_pn81@28-1>
2024-09-04 11:39:10 +0300

    class Point

--- attempt2/FileName.cpp
+++ attempt2/FileName.cpp
@@ -1,9 +1,52 @@
 #include <iostream>
 using namespace std;
 
+class Point
+{
+public:
+	void Print()
+	{
+		cout << "X: " << x << "\tY: " << y << endl;
+	}
+
+	void Init(int a, int b)
+	{
+		x = a;
+		y = b;
+	}
+
+	int GetX()
+	{
+		return x;
+	}
+	int GetY()
+	{
+		return y;
+	}
+
+private:
+
+	int x;
+	int y;
+
+};
+
 
 
 int main()
 {
-	cout << "Hello world" << endl;
+	
+	Point a;
+	a.Init(1, 2);
+	a.Print();
+
+	Point b;
+	b.Init(10, 20);
+	b.Print();
+
+	cout << a.GetX() << endl; 
+	cout << b.GetX() << endl;
+
+
+
 }
